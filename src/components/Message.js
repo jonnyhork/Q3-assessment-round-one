46146ed7a34b21693fc0630b86a2eda337bd28d5
Jonny Hork <JonnyHork@gmail.com>
2017-11-10 08:24:02 -0700

    Name of sender will render.

--- src/components/Message.js
+++ src/components/Message.js
@@ -4,7 +4,13 @@ import React from 'react'
   const Message = ({ message }) => {
     
     return (
-      <li className="list-group-item list-group-item-info d-flex justify-content-between"><span><p className="font-weight-bold">{message.name} Says:</p></span>{message.message}
+      <li className="list-group-item list-group-item-info d-flex justify-content-between">
+        <span>
+          <p className="font-weight-bold">
+            {message.name} says:
+          </p>
+        </span>
+            {message.message}
         <button className="btn btn-danger">
           <i className="fa fa-trash-o" aria-hidden="true"></i>
         </button>

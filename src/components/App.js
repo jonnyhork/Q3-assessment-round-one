eb0b48d858cc4e84c7daa40be9cb024ed6a3fd38
Jonny Hork <JonnyHork@gmail.com>
2017-11-09 22:29:27 -0700

    First commit

---
+++ src/components/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import logo from '../logo.svg';
+import '../App.css';
+
+class App extends Component {
+  
+  state = {
+    messages:[],
+    searchMessages:[]
+  }
+  
+  // async componentDidMount()
+  
+  // get messages method
+    // ${REACT_APP_API_URL}/messages
+  
+  
+  render() {
+    return (
+      <h3>Jonny</h3>
+    );
+  }
+}
+
+export default App;

a77e8459e65825fc5381f2d802e77baee265bb89
Jonny Hork <JonnyHork@gmail.com>
2017-11-10 09:44:01 -0700

    search bar will set state to its value

--- src/components/ToolBar.js
+++ src/components/ToolBar.js
@@ -9,14 +9,27 @@ class ToolBar extends Component {
     }
   }
   
+  onSearchInputChange(searchTerm) {
+    this.setState({
+      searchTerm
+    })
+    // this.props.searchbar term function to actually search
+  }
+  
   render() {
     return (
     <div className="row">
       <div className="col-md-6">
+        the search term is: {this.state.searchTerm}
         <div className="input-group p-3">
-          <input type="text" className="form-control" placeholder="Search" aria-label="Search for a name or message"/>
+          <input type="text" className="form-control" placeholder="Search" aria-label="Search for a name or message"
+          value={this.state.searchTerm}
+          onChange= {
+            (e) => this.onSearchInputChange((e.target.value))
+          }
+          />
           <span>
-            <button onClick={()=>this.props.toggleCompose()} className="btn btn-info" type="button">
+            <button onClick={() => this.props.toggleCompose()} className="btn btn-info" type="button">
               <i className={`fa fa-plus`}></i>
               Add
             </button>
